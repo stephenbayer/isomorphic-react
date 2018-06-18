4b6479af9ab9c3cf446b5f272bd9f82ec8de495e
Stephen Bayer <sabayer@gmail.com>
2018-06-18 12:10:42 -0700

    adding tests with mocks for fetch

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,8 +1,15 @@
 import { handleFetchQuestion } from "./fetch-question-saga";
+import fetch from 'isomorphic-fetch';
 
 describe('Fetch questions saga', () =>{
-  it("should fetch the question", () => {
+  beforeAll(() => {
+    fetch.__setValue([{question_id: 42}]);
+  });
+  it("should fetch the question", async () => {
     console.log("Test running");
-    handleFetchQuestion()
+    const gen = handleFetchQuestion({question_id: 42});
+    const { value } = await gen.next();
+    expect(value).toEqual([{question_id: 42}]);
+    expect(fetch).toHaveBeenCalledWith('/api/questions/42');
   });
 });
